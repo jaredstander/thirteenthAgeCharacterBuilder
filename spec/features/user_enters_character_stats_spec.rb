0091b9596144431b547a1217ede4512b3a098ce4
Jared Stander <jaredstander@gmail.com>
2016-05-16 14:41:31 -0400

    Appending additional stats tests for creating characters.

--- spec/features/user_enters_character_stats_spec.rb
+++ spec/features/user_enters_character_stats_spec.rb
@@ -1,12 +1,15 @@
 # Jared, this goes in spec/features/user_enters_character_stats_spec.rb
 
-# STATS - Name, Race, Stats, Base Init
-# BIOS - Backgrounds, One Unique Things, Icon Relationships
-# CLASS - Class, Feats, Class Talents / Features, Recoveries, HP, AC
+# STATS - Name, Race, Physical Stats
+# BIOS - Backgrounds, One Unique Things
+# CLASS - Class, Feats, Class Talents / Features
 # SPELLS - Spell lists
 # WEAPONS
 # INVENTORY
 # WEALTH
+
+#Things that will be filled-in qutomatically:
+# Init, Race Ability, Mental Defense, Physical Defense, Recovery and Recovery Die, AC, HP
 
 require 'rails_helper'
 
@@ -14,15 +17,14 @@ feature 'User enters character stats' do
   scenario 'successfully' do
     visit new_stat_path
 
-    fill_in "stats_character_name", with: 'thing'
-    select 'Dark Elf', from: 'race'
-    fill_in 'strength', with: 10
-    fill_in 'dexterity', with: 6
-    fill_in 'constitution', with: 12
-    fill_in 'wisdom', with: 8
-    fill_in 'intelligence', with: 14
-    fill_in 'charisma', with: 10
-    fill_in 'initiative', with: -2
+    fill_in "character name", with: 'thing'
+    select 'Dark Elf', from: 'stat_character_race'
+    fill_in 'stat_character_strength', with: 10
+    fill_in 'stat_character_dexterity', with: 6
+    fill_in 'stat_character_constitution', with: 12
+    fill_in 'stat_character_wisdom', with: 8
+    fill_in 'stat_character_intelligence', with: 14
+    fill_in 'stat_character_charisma', with: 10
     click_on 'Create'
 
     # Jared, these are the derived, computed stats
